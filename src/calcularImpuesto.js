97dd7754c25c23d4d000655e401dd01e688e0044
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-03-02 16:44:40 -0400

    se anadio la funcionalidad de calcular el impuesto para Nevada segun el precio neto

--- src/calcularImpuesto.js
+++ src/calcularImpuesto.js
@@ -5,6 +5,10 @@ function calcularImpuesto(precioNeto,estado)
    {
       return (precioNeto*8.25)/100;
    }
+   if(estado=="NV")
+   {
+      return (precioNeto*8.00)/100;
+   }
 }
 
 export default calcularImpuesto;
--- src/totalizador.spec.js
+++ src/totalizador.spec.js
@@ -8,5 +8,7 @@ describe("Totalizar", () => {
   it("deberia calcular el impuesto para California segun el precio neto", () => {
     expect(calcularImpuesto(49, "CA")).toEqual(4.0425);
   });
-  
+  it("deberia calcular el impuesto para Nevada segun el precio neto", () => {
+    expect(calcularImpuesto(49, "NV")).toEqual(3.92);
+  });
 });
