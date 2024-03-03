a3e9545f3aefdf5d231c4aede3474940593f1d67
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-03-03 16:26:22 -0400

    refactorizando la funcion calcularPesoVolumetrico

--- src/calcularPesoVolumetrico.js
+++ src/calcularPesoVolumetrico.js
@@ -1,32 +1,17 @@
 function calcularPesoVolumetrico(cantidad,peso)
 {
-    if(peso=="0-10")
-    {
-        return cantidad*0;
+    let pesosYenvios={
+        "0-10":0,
+        "11-20":3.5,
+        "21-40":5,
+        "41-80":6,
+        "81-100":6.5,
+        "101-200":8,
+        ">200":9
     }
-    if(peso=="11-20")
-    {
-        return cantidad*3.5;
-    }
-    if(peso=="21-40")
-    {
-        return cantidad*5;
-    }
-    if(peso=="41-80")
-    {
-        return cantidad*6;
-    }
-    if(peso=="81-100")
-    {
-        return cantidad*6.5;
-    }
-    if(peso=="101-200")
-    {
-        return cantidad*8;
-    }
-    else
-    {
-        return cantidad*9;
-    }
+    if(peso in pesosYenvios)
+   {
+      return cantidad*pesosYenvios[peso];
+   }
 }
 export default calcularPesoVolumetrico;
