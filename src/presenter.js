3af398b921e6e4143eac05acd1454543d0fe2f2a
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-03-02 21:18:02 -0400

    se anadio el diccionario categorias productos con sus impuestos y descuentos para mostrarlo en el index.html al elegir una categoria

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,12 @@
 
 import calcularPrecioNeto from "./calcularPrecioNeto";
 import calcularImpuesto from "./calcularImpuesto";
+import categoriasProductos from "./productos";
 import calcularDescuento from "./calcularDescuento";
 const cantidadItems_input = document.querySelector("#cantidad-items");
 const precioItems_input= document.querySelector("#precio-items");
 const opcionesSelect = document.querySelector('#opciones');
+const opcionesCategoria = document.querySelector('#categoria');
 const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 
@@ -14,6 +16,7 @@ form.addEventListener("submit", (event) => {
   const cantidad = cantidadItems_input.value;
   const precio = precioItems_input.value;
   const estado = opcionesSelect.value;
+  const categoria=opcionesCategoria.value;
   const precioNeto = calcularPrecioNeto(cantidad, precio);
   const descuento=calcularDescuento(precioNeto);
   const impuesto = calcularImpuesto(precioNeto,estado);
@@ -22,5 +25,14 @@ form.addEventListener("submit", (event) => {
   div.innerHTML += "<p>" + "Impuesto para " + estado +":"+ impuesto +"$"+ "</p>";
   const precioTotal=precioNeto+impuesto-descuento;
   div.innerHTML += "<p>" + "Precio total (impuesto, descuento): "+ precioTotal +"$"+ "</p>";
+  let tipoProductoEscogido;
+  console.log(categoria);
+  if(categoria in categoriasProductos)
+  {
+    tipoProductoEscogido=categoriasProductos[categoria];
+    console.log(tipoProductoEscogido);
+    div.innerHTML += "<p>" + "Impuesto y descuento para la categoria "+ categoria + "(" + tipoProductoEscogido.Impuesto+"% "+" - "+tipoProductoEscogido.Descuento +" % "+ ")" +"</p>";
+  }
+  
   
 });
---
+++ src/productos.js
@@ -0,0 +1,43 @@
+// Crear un diccionario de personas
+let categoriasProductos =
+{
+    Escritorio: 
+    {
+      Impuesto: 0,
+      Descuento:1.5 
+    },
+    Varios: 
+    {
+      Impuesto: 0,
+      Descuento: 0
+    },
+    Muebles: 
+    {
+      Impuesto: 3,
+      Descuento: 0
+    },
+    Vestimenta:
+     {
+        Impuesto: 2,
+        Descuento:0 
+      },
+      Electronicos: 
+      {
+        Impuesto: 4,
+        Descuento: 1
+      },
+      Bebidas: 
+      {
+        Impuesto: 7,
+        Descuento: 0
+      },
+      Alimentos: 
+      {
+        Impuesto: 0,
+        Descuento: 2
+      }
+  };
+
+  export default categoriasProductos;
+
+  
